1f98fc96e8ce43db46e7021fe04a07e6e55545c9
fishcried <fishcried@163.com>
2015-03-30 06:14:50 +0000

    整理了快速生成博客的命令

--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -6,62 +6,81 @@
 
 
 
-function openstack_workspace()
+function load_openstack()
 {
-	SNAME=$1
+	local workspace=${FUNCNAME##*_}
 
-	tmux has-session -t $SNAME
+	tmux has-session -t $workspace
 	if [ $? != 0 ]; then
 		# (1) load ssh keys
-		tmux new-session -s $SNAME -n Load-ssh-keys -d
-		tmux send-keys -t $SNAME:1  'load-ssh-keys.sh' C-m
+		tmux new-session -s $workspace -n Load-ssh-keys -d
+		tmux send-keys -t $workspace:1  'load-ssh-keys.sh' C-m
 
 		# (2) Login Config Node
-		tmux new-window -t $SNAME -n Config-Node
-		tmux send-keys -t $SNAME:2  'ssh ubuntu@192.168.250.20' C-m
+		tmux new-window -t $workspace -n Config-Node
+		tmux send-keys -t $workspace:2  'ssh ubuntu@192.168.250.20' C-m
 
 		# (3) Login Keystone Node
-		tmux new-window -t $SNAME -n Keystone-Node
-		tmux send-keys -t $SNAME:3  'ssh ubuntu@192.168.250.11' C-m
+		tmux new-window -t $workspace -n Keystone-Node
+		tmux send-keys -t $workspace:3  'ssh ubuntu@192.168.250.11' C-m
 
 		# (4) Login Network Node
-		tmux new-window -t $SNAME -n Network-Node
-		tmux send-keys -t $SNAME:4  'ssh ubuntu@192.168.250.16' C-m
+		tmux new-window -t $workspace -n Network-Node
+		tmux send-keys -t $workspace:4  'ssh ubuntu@192.168.250.16' C-m
 
-		tmux select-window -t $SNAME:2
+		tmux select-window -t $workspace:2
 	fi
 }
 
-function normal_worksapce()
+function load_working()
 {
-	SNAME=$1
+	local workspace=${FUNCNAME##*_}
 
-	tmux has-session -t $SNAME
+	tmux has-session -t $workspace
 
 	if [ $? != 0 ]; then
 		# (1)WIN
-		tmux new-session -s $SNAME -n WIN -d
-		tmux send-keys -t $SNAME:1 'win7' C-m
+		tmux new-session -s $workspace -n Timing -d
+		tmux send-keys -t $workspace:1  'time read' C-m
 
-		# (2/1)BLOG/Jekyll server
-		tmux new-window -t $SNAME -n BLOG
-		tmux send-keys -t $SNAME:2  'jump blog && jekyll server -D --watch -H 0.0.0.0' C-m
-		# (2/2)BLOG/Jekyll Editor
-		#tmux split-window -v -t $SNAME:2
-		#tmux send-keys  -t $SNAME:2.2 'jump blog' C-m
+		# (2) Working
+		tmux new-window -t $workspace -n Working
 
-		# (3) Timing
-		tmux new-window -t $SNAME -n Timing
-		tmux send-keys -t $SNAME:3  'time read' C-m
-
-		# (4) Working
-		tmux new-window -t $SNAME -n Working
-
-		tmux select-window -t $SNAME:4
+		tmux select-window -t $workspace:2
 	fi
 }
 
-normal_worksapce fishcried
-openstack_workspace openstack
+function load_blog() {
+		
+	local workspace=${FUNCNAME##*_}
 
-tmux attach -t fishcried
+	tmux has-session -t $workspace
+
+	if [ $? != 0 ]; then
+		# (1)drafts
+		tmux new-session -s $workspace -n Drafts -d
+		tmux send-keys -t $workspace:1 'jump draft && jekyll server -D --watch -H 0.0.0.0 -P 4001' C-m
+
+		# (2)server
+		tmux new-window -t $workspace -n Blog
+		tmux send-keys -t $workspace:2  'jump blog && jekyll server --watch -H 0.0.0.0 -P 4000' C-m
+	fi
+}
+
+SPACES="working \
+	    blog
+        openstack"
+
+if [ $# -eq 0 ];then
+	for space in $SPACES
+	do
+		load_$space
+	done
+	tmux attach -t working
+else
+	for space in $*
+	do
+		load_$space
+	done
+	tmux attach -t $1
+fi
